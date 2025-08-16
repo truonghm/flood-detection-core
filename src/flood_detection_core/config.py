5689f6fbfc34dfc150aab7514efb6e10797eb355
truonghm <truong173@outlook.com>
2025-08-16 22:34:32 +0700

    refact: increase patches amount to all to see if it improves performance

--- src/flood_detection_core/config.py
+++ src/flood_detection_core/config.py
@@ -222,7 +222,7 @@ class CLVAESiteSpecificConfig(BaseModel):
     # Loss weights from paper
     alpha: float = 0.1
     beta: float = 0.7
-    max_patches_per_pair: int = 128
+    max_patches_per_pair: int | Literal["all"] | None = "all"
 
 
 class CLVAEInferenceConfig(BaseModel):
